7b12dc3f8c504fabfaae37f52834d112f66915bc
Sven <Svenluecke48@gmx.de>
2016-04-02 12:28:18 +0200

    added tests for generated html

--- tests/integration/components/swiper-container-test.js
+++ tests/integration/components/swiper-container-test.js
@@ -6,19 +6,31 @@ moduleForComponent('swiper-container', 'Integration | Component | swiper contain
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{swiper-container}}`);
-
   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
   this.render(hbs`
     {{#swiper-container}}
       template block text
     {{/swiper-container}}
   `);
-
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('predefined classes are added', function(assert) {
+  this.render(hbs`{{#swiper-container}} Foo {{/swiper-container}}`);
+  assert.ok(this.$('>:first-child').hasClass('swiper-container'));
+});
+
+test('contains the wrapper', function(assert) {
+  this.render(hbs`{{#swiper-container}} Foo {{/swiper-container}}`);
+  assert.ok(this.$('>:first-child').has('.swiper-wrapper').length);
+});
+
+test('pagination node is present if requested', function(assert) {
+  this.render(hbs`{{#swiper-container pagination=false}} Foo {{/swiper-container}}`);
+  assert.notOk(this.$('>:first-child').has('.swiper-pagination').length);
+
+  this.render(hbs`{{#swiper-container pagination=true}} Foo {{/swiper-container}}`);
+  assert.ok(this.$('>:first-child').has('.swiper-pagination').length);
 });
--- tests/integration/components/swiper-slide-test.js
+++ tests/integration/components/swiper-slide-test.js
@@ -6,19 +6,27 @@ moduleForComponent('swiper-slide', 'Integration | Component | swiper slide', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{swiper-slide}}`);
-
   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
   this.render(hbs`
     {{#swiper-slide}}
       template block text
     {{/swiper-slide}}
   `);
-
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('predefined classes are added', function(assert) {
+  this.render(hbs`{{#swiper-slide}} Foo {{/swiper-slide}}`);
+  assert.ok(this.$('>:first-child').hasClass('swiper-slide'));
+
+  this.render(hbs`{{#swiper-slide class='foo bar'}} Foo {{/swiper-slide}}`);
+  assert.ok(this.$('>:first-child').hasClass('swiper-slide'));
+});
+
+test('own classes are added', function(assert) {
+  this.render(hbs`{{#swiper-slide class='foo bar'}} Foo {{/swiper-slide}}`);
+  assert.ok(this.$('>:first-child').hasClass('foo'));
+  assert.ok(this.$('>:first-child').hasClass('bar'));
 });
