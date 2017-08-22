Ember-Swiper/ember-cli-swiper

commit d426a4f1e1a7ca7de2742d203f8b615198822cc9

Ember Fastboot rc.1 support (#56)
* ember fastboot rc.1 support. Also removed bower dep.

* don't mess with styles

* destroy blueprint

index.js
@@ -1,32 +1,35 @@
 /* jshint node:true */
 'use strict';
+var map = require('broccoli-stew').map;
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-swiper',
 
-  included(app) {
+  treeForVendor: function(defaultTree) {
+    var swiperPath = path.join(path.dirname(require.resolve('swiper')), '..');
+    var browserVendorLib = new Funnel(swiperPath, {
+      destDir: 'swiper',
+      include: ['js/swiper.min.js', 'css/swiper.min.css']
+    });
+
+    browserVendorLib = map(browserVendorLib, (content, relativePath) => {
+      if (relativePath.indexOf('css') !== -1) {
+        return content;
+      }
+      return `if (typeof FastBoot === 'undefined') { ${content} }`
+    });
+  
+    return new mergeTrees([defaultTree, browserVendorLib]);
+  },
+
+  included: function(app) {
     this._super.included.apply(this, arguments);
-    
-    var app;
 
-    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
-    // use that.
-    if (typeof this._findHost === 'function') {
-      app = this._findHost();
-    } else {
-      // Otherwise, we'll use this implementation borrowed from the _findHost()
-      // method in ember-cli.
-      var current = this;
-      do {
-        app = current.app || app;
-      } while (current.parent.parent && (current = current.parent));
-    }
-
-    app.import(app.bowerDirectory + '/swiper/dist/css/swiper.min.css');
-
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(app.bowerDirectory + '/swiper/dist/js/swiper.min.js');
-    }
+    this.app.import('vendor/swiper/js/swiper.min.js');
+    this.app.import('vendor/swiper/css/swiper.min.css');
   }
 
 };

blueprints/ember-cli-swiper/index.js
@@ -1,11 +1,0 @@
-module.exports = {
-  description: 'add swiper bower package',
-
-  normalizeEntityName() {
-  },
-
-  afterInstall() {
-    return this.addBowerPackageToProject('swiper', '~3.4.1 ');
-  }
-
-};

commit 10ef747840ff203147555fea534c275e28d46841

Fix: defaultTree may be undefined
Issue was introduced by #56.

index.js
@@ -9,6 +9,7 @@ module.exports = {
   name: 'ember-cli-swiper',
 
   treeForVendor: function(defaultTree) {
+    var trees = [];
     var swiperPath = path.join(path.dirname(require.resolve('swiper')), '..');
     var browserVendorLib = new Funnel(swiperPath, {
       destDir: 'swiper',
@@ -21,8 +22,14 @@ module.exports = {
       }
       return `if (typeof FastBoot === 'undefined') { ${content} }`
     });
-  
-    return new mergeTrees([defaultTree, browserVendorLib]);
+
+    if (defaultTree !== undefined) {
+      trees.push(defaultTree);
+    }
+
+    trees.push(browserVendorLib);
+
+    return new mergeTrees(trees);
   },
 
   included: function(app) {