ba3fb077305bd6977b716f8321ac9dfa681753c3
Inez Korczyński <korczynski@gmail.com>
2017-09-06 14:13:33 -0700

    Fix: followFinger by default is true (#66)

--- addon/components/swiper-container.js
+++ addon/components/swiper-container.js
@@ -58,7 +58,7 @@ export default Component.extend({
     }
 
     // Disables swiping
-    if (this.get('followFinger')) {
+    if (this.get('followFinger') === false) {
       options.followFinger = false;
     }
 
