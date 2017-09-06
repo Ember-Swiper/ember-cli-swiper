8a022983c7ae49d080ed6e8c878b4ecac094d142
Matt-Jensen <matt@jwc.works>
2017-09-06 09:32:01 -0500

    Upgrade ember-cli to 2.15.1 (#70)
    * upgrade ember-cli to 2.15.1

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,11 +3,8 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const { Application } = Ember;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-const App = Application.extend({
+// jscs:disable disallowDirectPropertyAccess
+const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
