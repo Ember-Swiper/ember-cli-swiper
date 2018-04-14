f9534d9a2e4b4c2fef7c381be78b76718204f5d0
Matt-Jensen <matt@jwc.works>
2018-04-14 16:07:46 -0500

    #97 migrate ember-suave to eslint-plugin-ember-suave
    Signed-off-by: Matt-Jensen <matt@jwc.works>

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'plugin:ember-suave/recommended'
   ],
   env: {
     browser: true
