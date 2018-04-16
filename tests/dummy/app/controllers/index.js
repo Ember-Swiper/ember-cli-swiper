86e8e5d4daa9cbc92b4e820e6d53fc0ccedafed4
Matt-Jensen <matt@jwc.works>
2018-04-16 16:44:53 -0500

    #80 add swiper events support, tests, and docs
    Signed-off-by: Matt-Jensen <matt@jwc.works>

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { later } from '@ember/runloop';
 import jQ from 'jquery';
 
 export default Controller.extend({
@@ -7,15 +8,16 @@ export default Controller.extend({
   currentSlide2: 2,
 
   actions: {
-
     addToHistory(newElement) {
       this.set('selectionHistory', `${jQ(newElement).text()}${this.get('selectionHistory')}`);
     },
 
     goToTwo() {
       this.set('currentSlide', 2);
+    },
+
+    onSwiperEnd() {
+      later(() => alert('End of slider'), 300);
     }
-
   }
-
 });
