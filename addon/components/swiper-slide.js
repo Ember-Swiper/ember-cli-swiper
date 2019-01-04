bea83b2819ac7d949328df8a16bc3eaee8ee0d53
@anulman <anulman@gmail.com>
2019-01-03 21:13:32 -0500

    add `data-` attributeBindings to swiper-slide components

--- addon/components/swiper-slide.js
+++ addon/components/swiper-slide.js
@@ -1,7 +1,25 @@
 import Component from '@ember/component';
 import layout from '../templates/components/swiper-slide';
 
+export const PERMITTED_DATA_ATTRIBUTES = [
+  'data-history',
+  'data-id',
+  'data-swiper-autoplay',
+  'data-swiper-parallax',
+  'data-swiper-parallax-x',
+  'data-swiper-parallax-y',
+  'data-swiper-parallax-scale',
+  'data-swiper-parallax-opacity',
+  'data-swiper-parallax-duration',
+  'data-background',
+  'data-src',
+  'data-srcset',
+  'data-swiper-zoom',
+  'data-hash'
+];
+
 export default Component.extend({
   layout,
+  attributeBindings: PERMITTED_DATA_ATTRIBUTES,
   classNames: ['swiper-slide']
 });
