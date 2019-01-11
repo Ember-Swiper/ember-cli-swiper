747ed9e1fad5b997dd324fe6a77d78ce1c89d1de
@anulman <anulman@gmail.com>
2019-01-10 18:08:51 -0800

    add ember template compiler tooling to test app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/ember/ember-template-compiler.js');
+
   /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
