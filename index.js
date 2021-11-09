28ecb03cb444e02e7427e2b84e33b33aee380e52
Szymon Tyminski <szymon@tyminski.cc>
2021-11-09 15:48:57 +0000

    Adds ember-auto-import

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
 'use strict';
-const map = require('broccoli-stew').map; // eslint-disable-line ember-suave/prefer-destructuring, node/no-extraneous-require
-const path = require('path');
-const Funnel = require('broccoli-funnel'); // eslint-disable-line node/no-extraneous-require
-const MergeTrees = require('broccoli-merge-trees'); // eslint-disable-line node/no-extraneous-require
 
 module.exports = {
-  name: 'ember-cli-swiper',
-
-  treeForVendor(defaultTree) {
-    let trees = [];
-    let swiperPath = path.join(path.dirname(require.resolve('swiper')), '..');
-    let browserVendorLib = new Funnel(swiperPath, {
-      destDir: 'swiper',
-      include: ['js/swiper.js', 'css/swiper.css']
-    });
-
-    browserVendorLib = map(browserVendorLib, (content, relativePath) => {
-      if (relativePath.indexOf('css') !== -1) {
-        return content;
-      }
-      return `if (typeof FastBoot === 'undefined') { ${content} }`;
-    });
-
-    if (defaultTree !== undefined) {
-      trees.push(defaultTree);
-    }
-
-    trees.push(browserVendorLib);
-
-    return new MergeTrees(trees);
-  },
-
-  included(/* app */) {
-    this._super.included.apply(this, arguments);
-    this.import('vendor/swiper/js/swiper.js');
-    this.import('vendor/swiper/css/swiper.css');
-  }
+  name: 'ember-cli-swiper'
 };
