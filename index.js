86f3ef3ce9ca3bd6fd730e2295ca09f58ada223f
Szymon Tyminski <szymon@tyminski.cc>
2021-11-16 13:20:44 +0000

    Imports stylesheet file

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 
 module.exports = {
   name: require('./package').name,
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('node_modules/swiper/swiper-bundle.css');
+  }
 };
